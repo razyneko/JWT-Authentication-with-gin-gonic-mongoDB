afe03569d7bdfb26ad18b0321b178fe82d6d51a2
Ayush Vishwakarma <139580532+razyneko@users.noreply.github.com>
2025-01-01 22:13:52 +0530

    Updated databaseConnection.go

--- database/databaseConnection.go
+++ database/databaseConnection.go
@@ -6,33 +6,30 @@ import (
 	"log"
 	"os"
 	"time"
-	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+// DBInstance() creates a mongo client
 func DBinstance() *mongo.Client{
-	err := godotenv.Load(".env")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	// creates a context with a deadline of 10 seconds from now. It ensures that any operation using this context will automatically be canceled if it exceeds the 10-second limit.
+
+	defer cancel()
+	// ensures that resources associated with the context are properly released when the function completes, avoiding potential resource leaks
+
+	clientOptions := options.Client().ApplyURI(os.Getenv("MONGO_URI"))
+
+	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatal("Error connecting to MongoDB")
 	}
 
-	MongoDb := os.Getenv("MONGODB_URL")
-	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDb))
+	// verify the connection
+	err = client.Ping(ctx, nil)
 	if err != nil{
-		log.Fatal(err)
+		log.Fatal("Couldn't connect to MongoDB")
 	}
-	// context timeout
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	err = client.Connect(ctx)
-	if err != nil{
-		log.Fatal(err)
-	}
-	if err != nil{
-		log.Fatal(err)
-	}
-	fmt.Println("Successfully connected to MongoDB")
 	return client
 }
 // exportable var for mongo client
